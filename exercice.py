a52b60d3d7f2c1fc826513a6341beaff04c7b7f6
ZakariaZair <zakaria.zair@polymtl.ca>
2021-10-25 12:52:57 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -2,13 +2,42 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
+import copy
+import math
+import turtle
+import matplotlib
 
 
 # TODO: Définissez vos fonction ici
+def volumeEpsiloide(demi_axe1, demi_axe2, demi_axe3, masse_volumique):
+    volume = (4/3)*math.pi*demi_axe1*demi_axe2*demi_axe3
+    masse = masse_volumique*volume
+    volume_et_masse = (volume, masse)
+    return volume_et_masse
+
+def arbre(n):
+    t = turtle
+    compteur = 0
+    x = 2.6
+    while True:
+        t.fd(x*100)
+        t.lt()
+        t.rt()
+        t.bk()
+        compteur += 1
+        if compteur % 4 == 0:
+            x += 0.7
+
+
+
+
+        
+
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
-    pass
+    print(volumeEpsiloide(2,3,4,0.294))
+    niveau = 3
+    arbre(niveau)
+    
